6d17330f09bf438984d14f3d56b29d02e0e80b53
Muhammad Rifki <mrifki028@gmail.com>
2023-03-10 05:36:45 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = hapi.server({
+    host: "localhost",
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada: ${server.info.uri}`);
+};
+
+init();
